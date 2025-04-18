32db6b09295f31f57fd01e0352a269c350332db1
./Freedom Security <oppokontol13@gmail.com>
2025-04-18 11:01:07 +0700

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+$sitename = 'A Simple  Leyer 7 - Layer 4 Dstat';
+$zone = 'RU';
+$datatype = 'RX';
+$lang = 'EN';
+$interface = 'eth0';
+$ip = "127.0.0.1";
+$port = "80";
+$Layer4Title = "Layer 4 => {$ip} PORT {$port} ";
+$Layer7Title = "Layer 7 =>{$ip} PORT {$port} ";
